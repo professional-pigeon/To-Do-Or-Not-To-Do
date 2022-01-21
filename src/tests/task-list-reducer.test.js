0e67ca6c3e095d85c99edb72a51facadf077f948
professional-pigeon <kyle.kayperez@gmail.com>
2022-01-21 15:27:21 -0800

    add return statement to task-list-reducer pass test to return blank state

--- src/tests/task-list-reducer.test.js
+++ src/tests/task-list-reducer.test.js
@@ -1,4 +1,4 @@
-import taskListReducer from '../../reducers/task-list-reducer'
+import taskListReducer from '../reducers/task-list-reducer'
 
 describe('taskListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', 
