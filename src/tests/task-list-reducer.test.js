aea18cbc8c803bfe133195510fc31fc8593d5d7d
professional-pigeon <kyle.kayperez@gmail.com>
2022-01-21 16:10:35 -0800

    add test to remove an item from the task list, test fails

--- src/tests/task-list-reducer.test.js
+++ src/tests/task-list-reducer.test.js
@@ -8,7 +8,24 @@ describe('taskListReducer', () => {
     description: "Must we always fidget over this widget? Let's not ditch it. Let's fix it and raise our resolution digits.",
     dueDate: "some date in a time format",
     created: "Timestamp",
-    id: "generate Random"
+    id: 1
+  }
+
+  const currentState = {
+    1:  {
+      name: "Fix that widget",
+      description: "Must we always fidget over this widget? Let's not ditch it. Let's fix it and raise our resolution digits.",
+      dueDate: "some date in a time format",
+      created: "Timestamp",
+      id: 1
+    },
+    2: {
+      name: "Feed Squirrels",
+      description: "I tried to keep them out of the bird feeder. I'm now trying appeasement strategies.",
+      dueDate: "some date in a time format",
+      created: "Timestamp",
+      id: 2
+    }
   }
   test('Should return default state if there is no action type passed into the reducer', 
     ()=> {
@@ -36,4 +53,21 @@ describe('taskListReducer', () => {
       }
     });
   });
+
+  test('Should delete a task from the task list', () => {
+    action= {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(taskListReducer(currentState, action)).toEqual({
+      2: {
+        name: "Feed Squirrels",
+        description: "I tried to keep them out of the bird feeder. I'm now trying appeasement strategies.",
+        dueDate: "some date in a time format",
+        created: "Timestamp",
+        id: 2
+      }
+    })
+  })
+  
 });
