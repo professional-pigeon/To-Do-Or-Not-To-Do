a2d28561b65e44904f0ab916c7b61dbdd184b1a7
professional-pigeon <kyle.kayperez@gmail.com>
2022-01-22 20:05:51 -0800

    pass test to check individual states of each property in the rootReducer.

--- src/tests/index-reducer.test.js
+++ src/tests/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../reducers/index';
+import { createStore } from 'redux';
+import taskListReducer from '../reducers/task-list-reducer'
+import displayStateReducer from "../reducers/display-state-reducer";
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
@@ -7,6 +12,14 @@ describe("rootReducer", () => {
       displayState: "default",
       taskList: {}
     });
+  });
+
+  test('Check that initial state of taskListReducer matches root reducer', () => {
+    expect(store.getState().taskList).toEqual(taskListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of displayState matches root reducer', () => {
+    expect(store.getState().displayState).toEqual(displayStateReducer(undefined, { type: null }));
   });
 
 });
