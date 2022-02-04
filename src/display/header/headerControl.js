67c1292aa2694c38ad88dd4093c79a7300dc5886
professional-pigeon <kyle.kayperez@gmail.com>
2022-02-04 14:44:25 -0800

    add links to navbar for linkedin, github, other projects

--- src/display/header/headerControl.js
+++ src/display/header/headerControl.js
@@ -10,14 +10,11 @@ function HeaderControl() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="https://github.com/professional-pigeon">Github</Nav.Link>
-            <Nav.Link href="#link">linkedIn</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <Nav.Link href="https://www.linkedin.com/in/kyle-kay-perez-911700214/">LinkedIn</Nav.Link>
+            <NavDropdown title="Hosted Projects" id="basic-nav-dropdown">
+              <NavDropdown.Item href="https://drum-machine-kp.herokuapp.com/">Drum Machine</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/professional-pigeon">Github</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
